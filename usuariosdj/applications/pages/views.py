2ee9efabebaf0f91ec0658b2ba0bba16cff8abb6
f0xt0xcity <manueloctsi@gmail.com>
2021-12-23 19:59:08 -0600

    required staff agregado

--- usuariosdj/applications/pages/views.py
+++ usuariosdj/applications/pages/views.py
@@ -1,9 +1,12 @@
 from django.urls import reverse_lazy
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+
 
 from applications.pages.forms import PageForm
 
@@ -11,6 +14,15 @@ from .models import Page
 from .forms import PageForm
 
 # Create your views here.
+class StaffRequiredMixin(object):
+    """
+    Este mixin requerirá que el usuario sea miembro del staff
+    """
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
 class PagesListView(ListView):
     model = Page
     template_name = 'pages/pages.html'
@@ -23,6 +35,7 @@ class PagesDetailView(DetailView):
     context_object_name = 'page'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateView(CreateView):
     model = Page
     template_name = 'pages/add.html'
@@ -31,6 +44,7 @@ class PageCreateView(CreateView):
     success_url = reverse_lazy('pages_app:pages')
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdateView(UpdateView):
     model = Page
     template_name = 'pages/update.html'
@@ -38,7 +52,7 @@ class PageUpdateView(UpdateView):
     form_class =PageForm
     success_url = reverse_lazy('pages_app:pages')
     
-
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(DeleteView):
     model = Page
     template_name = 'pages/delete.html'
