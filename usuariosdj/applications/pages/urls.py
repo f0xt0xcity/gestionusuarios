f4a59f7fa2042f6ef2893b1ce3c83589d4b2ce6f
f0xt0xcity <manueloctsi@gmail.com>
2021-12-23 18:58:51 -0600

    Crud completo para paginas

--- usuariosdj/applications/pages/urls.py
+++ usuariosdj/applications/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PageCreateView, PagesDetailView, PagesListView
+from .views import PageCreateView, PageDeleteView, PageUpdateView, PagesDetailView, PagesListView
 
 app_name = 'pages_app'
 
@@ -17,5 +17,15 @@ urlpatterns = [
         'create/', 
         PageCreateView.as_view(), 
         name='page_create'
+        ),
+    path(
+        'update/<int:pk>/', 
+        PageUpdateView.as_view(), 
+        name='page_update'
+        ),
+    path(
+        'delete/<int:pk>/', 
+        PageDeleteView.as_view(), 
+        name='page_delete'
         ),
 ]
--- usuariosdj/applications/pages/views.py
+++ usuariosdj/applications/pages/views.py
@@ -2,6 +2,8 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 from .models import Page
 
@@ -22,4 +24,17 @@ class PageCreateView(CreateView):
     model = Page
     template_name = 'pages/add.html'
     fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages_app:pages')
+
+
+class PageUpdateView(UpdateView):
+    model = Page
+    template_name = 'pages/update.html'
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages_app:pages')
+    
+
+class PageDeleteView(DeleteView):
+    model = Page
+    template_name = 'pages/delete.html'
     success_url = reverse_lazy('pages_app:pages')
